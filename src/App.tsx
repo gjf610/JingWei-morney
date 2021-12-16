9cca0dbaed7af0f054f216a28e75ad9701d70d7d
jianfeng <gjf610@yeah.net>
2021-12-16 18:15:10 +0800

    feat: money TagsSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import React from "react";
-import {
-  HashRouter,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import './index.scss'
 import "./helper.scss";
 import Tags from "./views/Tags";
 import Money from "./views/Money";
 import Statistics from "./views/Statistics";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/tags">
-          <Tags />
-        </Route>
-        <Route path="/money">
-          <Money />
-        </Route>
-        <Route path="/statistics">
-          <Statistics />
-        </Route>
-        <Redirect exact from="/" to="/money" />
-        <Route path="*" >
-          <NoMatch />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <AppWrapper>
+      <HashRouter>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*" >
+            <NoMatch />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </AppWrapper>
   );
 }
 function NoMatch() {
