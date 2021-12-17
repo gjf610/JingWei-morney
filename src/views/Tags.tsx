81992cf916b7a80d0d14b278238131e1c1e1883c
jianfeng <gjf610@yeah.net>
2021-12-17 15:34:01 +0800

    fix: mul createId

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -51,7 +51,7 @@ function Tags() {
         {tags.map(tag =>
           <li key={tag.id}>
             <Link to={'/tags/' + tag.id}>
-              <span className="oneLine">{tag.name}</span>
+              <span className="oneLine">{tag.id}{tag.name}</span>
               <SvgIcon name="right" />
             </Link>
           </li>)}
