3797e16f69d4d666834e345666372a20eb0039e2
jianfeng <gjf610@yeah.net>
2021-12-17 14:44:26 +0800

    feat: added page Tags UI

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,11 +1,62 @@
+import React from "react";
 import { useTags } from "../useTags";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+import SvgIcon from "../components/SvgIcon";
 
+const TagList = styled.ol`
+  font-size: 16px;
+  background: #fff;
+  > li{
+    /* //#E5E5E7 */
+    margin:0 16px;
+    padding: 12px 16px 12px 0;
+    border-bottom: 1px solid #D5D5D9;
+    line-height: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    >.icon{
+        width: 20px;
+        height: 20px;;
+    }
+  }
+`
+const Button = styled.button`
+  border: none;
+  border-radius: 4px;
+  padding: 8px 12px;
+  font-size: 18px;
+  color: #fff;
+  background: #767676;
+`
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const Space = styled.div`
+  height: 16px;
+`
 function Tags() {
   const { tags, setTags } = useTags()
   return (
     <Layout>
-      <h2>标签页</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <SvgIcon name="right" />
+          </li>)}
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+        <Space />
+      </Center>
     </Layout>
   );
 }
