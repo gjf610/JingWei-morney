0a5b4db2d61a5a8c860092b26e71c7a0c11529f5
jianfeng <gjf610@yeah.net>
2021-12-18 15:44:07 +0800

    refactor: updateTag & deleteTag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,13 +21,13 @@ const useTags = () => {
     }
     return result
   }
-  const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id)
-    const tagsClone = JSON.parse(JSON.stringify(tags))
-    tagsClone.splice(index, 1, { id, name: obj.name })
-    setTags(tagsClone);
+  const updateTag = (id: number, { name }: { name: string }) => {
+    setTags(tags.map(tag => tag.id === id ? { id, name } : tag));
   }
-  return { tags, setTags, findTag, findTagIndex, updateTag }
+  const deleteTag = (id: number) => {
+    setTags(tags.filter(tag => tag.id !== id))
+  }
+  return { tags, setTags, findTag, findTagIndex, updateTag, deleteTag }
 }
 
 export { useTags }
