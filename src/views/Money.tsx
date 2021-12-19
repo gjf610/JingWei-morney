50dbe07ea52b02b7244b259abab54b5c2dbf9f6c
jianfeng <gjf610@yeah.net>
2021-12-19 22:42:43 +0800

    feat: mini screen to bottom

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -26,7 +26,7 @@ const defaultFormData = {
 
 function Money() {
   const [selected, setSelected] = useState(defaultFormData)
-  const { records, addRecordItem } = useRecords()
+  const { addRecordItem } = useRecords()
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({
       ...selected,
@@ -40,7 +40,7 @@ function Money() {
     }
   }
   return (
-    <MoneyLayout>
+    <MoneyLayout scrollTop={9999}>
       <TagsSection value={selected.tagIds}
         onChange={(tagIds) => onChange({ tagIds })} />
       <NoteSection value={selected.note}
