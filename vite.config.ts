663a758357d5f45693429d57a37ba11b906301de
jianfeng <gjf610@yeah.net>
2022-04-20 15:32:44 +0800

    deploy: remove morney-website

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { svgBuilder } from './src/plugins/svgBuilder'
 
 export default defineConfig({
-  base: '/morney-website/',
+  base: './',
   plugins: [
     react(),
     svgBuilder('./src/icons/')
